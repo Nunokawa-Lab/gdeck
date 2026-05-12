22af06414400db9d458119c2b07816705465e0f6
uw.rw.love@gmail.com <nun.k.biz@gmail.com>
2026-05-13 01:18:59 +0900

    feat: deleteコマンドも一括実行対応

--- cmd/internal/store/delete.go
+++ cmd/internal/store/delete.go
@@ -3,6 +3,7 @@ package store
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -18,9 +19,27 @@ func Delete(name string) error {
 		return fmt.Errorf("invalid name")
 	}
 
-	err = os.Remove(path)
-	if err != nil {
-		return fmt.Errorf("invalid name")
+	if strings.Contains(name, "*") {
+		// ディレクトリ内全件処理（ワイルドカードだったパスから各ファイルパスのスライスを取得）
+		paths, err := filepath.Glob(path)
+		if err != nil {
+			fmt.Println("ここ？")
+			return fmt.Errorf("invalid path")
+		}
+
+		for _, p := range paths {
+			err = os.Remove(p)
+			if err != nil {
+				return fmt.Errorf("request not found: %s", p)
+			}
+		}
+
+	} else {
+		err = os.Remove(path)
+		if err != nil {
+			fmt.Println("これ？")
+			return fmt.Errorf("invalid name")
+		}
 	}
 
 	return nil
